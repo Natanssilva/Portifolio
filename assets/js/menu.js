e4a84e43c1964dfab28c203ae315077ec39d2423
Natan <99294586+Natanssilva@users.noreply.github.com>
2023-05-17 18:01:18 -0300

    Add files via upload

---
+++ assets/js/menu.js
@@ -0,0 +1,16 @@
+
+
+const ButtonCheckBox = document.getElementById('ck');
+
+ButtonCheckBox.addEventListener('change', function DarkMode() {
+    document.body.classList.toggle('dark')
+})
+
+/*window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('#home', {duration: 1000});
+sr.reveal('#about', {duration: 1000});
+sr.reveal('#skills-container', {duration: 1000});
+sr.reveal('#projects', {duration: 1000});
+ 
+*/
